6629a70b00676dcc698e27b65a390e549c36396a
gnishida <gnishida@purdue.edu>
2019-11-28 18:45:15 -0800

    Apply augmenmtaion

--- tensorflow/nn_floor.py
+++ tensorflow/nn_floor.py
@@ -1,8 +1,11 @@
 import datetime
 import os
-from PIL import Image
+from PIL import Image, ImageDraw
 import numpy
 import glob
+import scipy
+import random
+from sklearn.utils import shuffle
 
 import tensorflow as tf
 from tensorflow import keras
@@ -20,33 +23,43 @@ WIDTH = 96
 NUM_CHANNELS = 3
 NUM_CLASSES = 1
 
-BASE_LEARNING_RATE = 0.01
+BASE_LEARNING_RATE = 0.001
 LR_SCHEDULE = [(0.1, 30), (0.01, 45)]
+
+IMG_NUM =  100
 
 
 def normalize(x, y):
-  x = tf.image.per_image_standardization(x)
-  return x, y
+	x = tf.image.per_image_standardization(x)
+	return x, y
 
 
-def augmentation(x, y):
-    x = tf.image.resize_with_crop_or_pad(
-        x, HEIGHT + 8, WIDTH + 8)
-    x = tf.image.random_crop(x, [HEIGHT, WIDTH, NUM_CHANNELS])
-    x = tf.image.random_flip_left_right(x)
-    return x, y	
+def augmentation(x):
+	#x = tf.image.resize_with_crop_or_pad(x, HEIGHT + 8, WIDTH + 8)
+	#x = tf.image.random_crop(x, [HEIGHT, WIDTH, NUM_CHANNELS])
+	x = tf.image.random_flip_left_right(x)
+	angle = random.uniform(-0.5, 0.5)
+	x = scipy.ndimage.rotate(x, angle , axes=(1, 0), reshape=False, order=3, mode='constant', cval=0.0, prefilter=True)
+	return x
+	
+
+def standardize_img(x):
+	mean = numpy.mean(x, axis=None, keepdims=True)
+	std = numpy.sqrt(((x - mean)**2).mean(axis=None, keepdims=True))
+	return (x - mean) / std
 
 
 def schedule(epoch):
-  initial_learning_rate = BASE_LEARNING_RATE * BS_PER_GPU / 128
-  learning_rate = initial_learning_rate
-  for mult, start_epoch in LR_SCHEDULE:
-    if epoch >= start_epoch:
-      learning_rate = initial_learning_rate * mult
-    else:
-      break
-  tf.summary.scalar('learning rate', data=learning_rate, step=epoch)
-  return learning_rate
+	initial_learning_rate = BASE_LEARNING_RATE * BS_PER_GPU / 128
+	learning_rate = initial_learning_rate
+	for mult, start_epoch in LR_SCHEDULE:
+		if epoch >= start_epoch:
+			learning_rate = initial_learning_rate * mult
+		else:
+			break
+	tf.summary.scalar('learning rate', data=learning_rate, step=epoch)
+	return learning_rate
+
 
 def load_img(file_path):
 	img = Image.open(file_path)
@@ -56,6 +69,43 @@ def load_img(file_path):
 	img = img.astype("float")
 	return img
 
+
+def load_imgs(path_list, use_augmentation = False, use_shuffle = False):
+	# Load image
+	
+	num_images = len(path_list)
+	if use_augmentation:
+		num_images *= IMG_NUM
+	
+	X = numpy.zeros((num_images, WIDTH, HEIGHT, 3), dtype=float)
+	Y = numpy.zeros((num_images), dtype=float)
+	
+	i = 0
+	for file_path in path_list:
+		imgx = load_img(file_path)
+		file_name = os.path.basename(file_path)
+		imgy = max(params[file_name])
+		if use_augmentation:
+			for j in range(IMG_NUM):
+				X[i * IMG_NUM + j,:,:,:] = augmentation(imgx)
+				X[i * IMG_NUM + j,:,:,:] = standardize_img(X[i * IMG_NUM + j,:,:,:])
+				Y[i * IMG_NUM] = imgy
+		else:
+			X[i,:,:,:] = standardize_img(imgx)
+			Y[i] = imgy
+		
+		i += 1
+		
+	if use_shuffle:
+		X, Y = shuffle(X, Y, random_state=0)
+	
+	# Create tensor from numpy array
+	#X = tf.constant(X)
+	#X = tf.compat.v2.image.per_image_standardization(X)
+	#Y = tf.constant(Y)
+
+	return X, Y
+	
 def load_annotation(file_path):
 	params = {}
 	file = open(file_path, "r")
@@ -71,7 +121,7 @@ def load_annotation(file_path):
 	return params
 
 
-def build_mode(int_shape, num_params):
+def build_model(int_shape, num_params):
 	model = keras.Sequential([
 		tf.keras.layers.Dense(64, activation='relu', input_shape=int_shape, name='fc1'),
 		tf.keras.layers.Dense(64, activation='relu', name='fc2'),
@@ -91,34 +141,17 @@ def build_mode(int_shape, num_params):
 params = load_annotation("facade_annotation.txt")
 
 
-# Load images
-path_list = glob.glob("../ECP/images/*.jpg")
-X = numpy.zeros((len(path_list), WIDTH, HEIGHT, 3), dtype=float)
-Y = numpy.zeros((len(path_list)), dtype=float)
-i = 0
-for file_path in path_list:
-	X[i,:,:,:] = load_img(file_path)
-	file_name = os.path.basename(file_path)
-	Y[i] = max(params[file_name])
-	i += 1
-
-
-# Create tensor from numpy array
-X = tf.constant(X)
-X = tf.compat.v2.image.per_image_standardization(X)
-Y = tf.constant(Y)
-
 # Split the tensor into train and test dataset
-num_train = int(len(Y) * 0.8)
-num_test = len(Y) - num_train
-trainX, testX = tf.split(X, [num_train, num_test], 0)
-trainY, testY = tf.split(Y, [num_train, num_test], 0)
+train_path_list = glob.glob("../ECP/image_train/*.jpg")
+trainX, trainY = load_imgs(train_path_list, use_augmentation = True, use_shuffle = False)
+test_path_list = glob.glob("../ECP/image_test/*.jpg")
+testX, testY = load_imgs(test_path_list)
 print(trainX.shape)
 print(testX.shape)
 
 
 # Build model
-model = build_mode((HEIGHT, WIDTH, NUM_CHANNELS), NUM_CLASSES)
+model = build_model((HEIGHT, WIDTH, NUM_CHANNELS), NUM_CLASSES)
 
 
 # Setup for Tensorboard
@@ -143,18 +176,33 @@ model.fit(trainX, trainY,
 model.evaluate(testX, testY)
 
 # Prediction
-predictedY = model.predict(X).flatten()
+predictedY = model.predict(testX).flatten()
+
 
 # Write the prediction to a file
 file = open("prediction.txt", "w")
-for i in range(len(path_list)):
-	file_name = os.path.basename(path_list[i])
-	#print("{}: {}".format(file_name, predictedY[i]))
+for i in range(len(test_path_list)):
+	file_name = os.path.basename(test_path_list[i])
 	file.write("{},{}\n".format(file_name, predictedY[i]))
 file.close()
 
+
+# Save the predicted images
+output_dir = "prediction_img"
+if not os.path.isdir(output_dir):
+	os.mkdir(output_dir)
+for i in range(len(test_path_list)):
+	file_name = os.path.basename(test_path_list[i])
+	img = Image.open(test_path_list[i])
+	w, h = img.size
+	imgdraw = ImageDraw.Draw(img)
+	imgdraw.line([(0, h * predictedY[i]), (w, h * predictedY[i])], fill = "yellow", width = 3)
+	img.save("{}/{}".format(output_dir, file_name))
+
+
+# Save the model
 model.save('model.h5')
 
+
+# Load the model
 #new_model = keras.models.load_model('model.h5')
- 
-#new_model.evaluate(testX, testY)
